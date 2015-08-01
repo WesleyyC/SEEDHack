e5e29b84259c872204c61006f607be9a1279f4c6
wesleyyc <wesley.chin0919@gmail.com>
2015-08-01 17:20:24 -0400

    Initial Commit

---
+++ app/src/main/java/com/seed/seedhackathon/aqualove/App.java
@@ -0,0 +1,20 @@
+package com.seed.seedhackathon.aqualove;
+
+/**
+ * Created by wesley on 7/24/15.
+ */
+import android.app.Application;
+import com.parse.Parse;
+
+public class App extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "K6gQKgy1awauF8bOgkkxu9D6Zblz9eFo2LlyLdIL", "FxWULU4zXI9tV7guHLb2Y82Sgl3gM271u9ve9QFg");
+
+    }
+}
